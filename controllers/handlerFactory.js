f9c381e6a6ba9ad49e20f832a085637f6b45c961
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-11-27 11:51:50 +0530

    Refactored Code and removed un-wanted files

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,13 +1,14 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appErrors");
-const APIFeatures = require("../utils/apiFeatures");
 
 exports.deleteOne = Model =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndDelete(req.params.id);
+    const doc = await Model.findByIdAndDelete(req.params.id); // _id = id parameter passed in the URL
 
+    //No document with that id exists
     if (!doc || doc == null)
       return next(new AppError("No document found with that ID", 404));
+    // Document deleted successfully, send acknowledgement
     res.status(204).json({
       status: "success",
       data: null,
@@ -16,13 +17,15 @@ exports.deleteOne = Model =>
 
 exports.updateOne = Model =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
     // https://mongoosejs.com/docs/api.html#model_Model-findByIdAndUpdate
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, // return new, updated document
+      runValidators: true, // run validation before updating document, ex: name must not be null,etc
+    });
+    //No document with that id exists
     if (!doc || doc == null)
       return next(new AppError("No document found with that ID", 404));
+    // Document updated successfully, send acknowledgement and the new document
     res.status(200).json({
       status: "success",
       data: {
@@ -33,7 +36,9 @@ exports.updateOne = Model =>
 
 exports.createOne = Model =>
   catchAsync(async (req, res, next) => {
+    //Create new document, from body passed in the req.body
     const newDoc = await Model.create(req.body);
+    // Document created successfully, send acknowledgement and the new document
     res.status(201).json({
       status: "success",
       data: {
@@ -44,36 +49,18 @@ exports.createOne = Model =>
 
 exports.getOne = (Model, populateOptions) =>
   catchAsync(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-    if (populateOptions) query = query.populate(populateOptions);
-    const doc = await query;
+    let query = Model.findById(req.params.id); // URL parameter id = id of existing user
+    if (populateOptions) query = query.populate(populateOptions); // If referencing is done, populate the data of referenced document inside of referring one
+    const doc = await query; // Run the query
     if (!doc || doc == null)
+      // No document exist with given id in the model
       return next(new AppError("No document found with that ID", 404));
+
+    // Document found return response
     res.status(200).json({
       status: "success",
       data: {
         data: doc,
       },
     });
-  });
-
-exports.getAll = Model =>
-  catchAsync(async (req, res, next) => {
-    let filter = {};
-    const features = new APIFeatures(Model.find(filter), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-
-    const doc = await features.query;
-    res.status(200).json({
-      status: "success",
-      length: doc.length,
-      data: {
-        data: doc,
-      },
-    });
-
-    //Get statistics about the query "features.query.explain()"
   });
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,16 @@ const sharp = require("sharp");
 const User = require("../models/userModel");
 const mongoose = require("mongoose");
 const catchAsync = require("../utils/catchAsync");
-const APIFeatures = require("../utils/apiFeatures");
 const AppError = require("../utils/appErrors");
 const factory = require("./handlerFactory");
 
 const multerStorage = multer.memoryStorage();
 
-//only allow images to be uploaded
+/*
+  |-------------------------------------------------------------------------|
+  |                 Only allow images to be uploaded                        |
+  |-------------------------------------------------------------------------|
+*/
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) cb(null, true);
   else cb(new AppError("Not an image!, Please upload images only!"), false);
@@ -17,7 +20,13 @@ const multerFilter = (req, file, cb) => {
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 exports.uploadUserPhoto = upload.single("photo");
 
-//minor-fixes to allow image-url from other sources i.e cloudinary
+/*
+  |-------------------------------------------------------------------------|
+  |         MAKE CHANGES TO UPLOADED FILE if any ex:resize, scale to        |
+  |         specific dimensions                                             |
+  |         Lastly store image at given location                            |
+  |-------------------------------------------------------------------------|
+*/
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
@@ -32,21 +41,28 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     .toFile(`public/img/users/${req.file.filename}`);
   next();
 });
+
+// Returns an object with allowedFields; fields which are allowed to be updated.
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach(key => {
+    //extract allowedFields from the obj
     if (allowedFields.includes(key)) {
       newObj[key] = obj[key];
     }
   });
-  return newObj;
+  return newObj; // return new obj containing only allowed fields.
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  // console.log(req.file);
-  // console.log(req.body);
+  // console.log(req.file); ==> User PFP
+  // console.log(req.body); ==> User data like: email, name, etc
 
-  // 1. Create error if user tries to update password
+  /*
+  |-------------------------------------------------------------------------|
+  |                1. Create error if user tries to update password         |
+  |-------------------------------------------------------------------------|
+*/
   if (req.body.password || req.body.passwordConfirm)
     return next(
       new AppError(
@@ -54,16 +70,27 @@ exports.updateMe = catchAsync(async (req, res, next) => {
         400
       )
     );
-  // 2. Filter out unwanted fields
-  const filteredBody = filterObj(req.body, "name", "email");
 
+  /*
+  |-------------------------------------------------------------------------|
+  |           2. Filter out unwanted fields                                 |
+  |-------------------------------------------------------------------------|
+*/
+  const filteredBody = filterObj(req.body, "name", "email"); //only name and email are allowed to be modified
+
+  // If an image is uploaded, add it to the object, containing values for fields to be updated
   if (req.file) {
     filteredBody.photo = req.file.filename;
   }
-  // 3. Update user document
+
+  /*
+  |-----------------------------------------------|
+  |          3. Update user document              |
+  |-----------------------------------------------|
+*/
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true,
-    runValidators: true,
+    new: true, // return the new, updated document
+    runValidators: true, // ex: check if name is not null, email is unique, etc. ==> Validations are defined within the DB Schemas
   });
   res.status(200).json({
     status: "success",
@@ -73,6 +100,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+/*
+  |-----------------------------------------------|
+  |      Delete the user, mark them as inactive   |
+  |-----------------------------------------------|
+*/
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
   res.status(204).json({
@@ -81,6 +113,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+/*
+  |-----------------------------------------------|
+  |    Set URL parameter id = id of current user  |
+  |-----------------------------------------------|
+*/
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: "error",
@@ -88,11 +130,16 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.getMe = (req, res, next) => {
-  req.params.id = req.user.id;
-  next();
-};
-exports.getAllUsers = factory.getAll(User);
-exports.getUser = factory.getOne(User);
-exports.updateUser = factory.updateOne(User);
-exports.deleteUser = factory.deleteOne(User);
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const doc = await User.find(); // Query the DB
+  res.status(200).json({
+    status: "success",
+    length: doc.length, // length of document
+    data: {
+      data: doc, // return doc
+    },
+  });
+});
+exports.getUser = factory.getOne(User); // Get a particular User
+exports.updateUser = factory.updateOne(User); // Update a  user
+exports.deleteUser = factory.deleteOne(User); // Delete a user
