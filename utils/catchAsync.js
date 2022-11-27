9742fb69f2c4015d8d62b5235925644daa236bd3
Uzair Saiyed <uzairhajra76330@gmail.com>
2022-11-27 21:33:30 +0530

    Fixed a major breaking change in the refactored code

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,6 @@
-module.exports = (fn) => {
-	return (req, res, next) => {
-		fn(req, res, next).catch(next);
-	};
+module.exports = fn => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(err => next(err));
+    // if first argument of next is of type Error, express automatically call the error handling middleware in errorController
+  };
 };
